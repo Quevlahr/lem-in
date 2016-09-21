f753c284d77d119aa2c2ebb1416a6ef0e413def8
Quentin Roulon <quroulon@student.42.fr>
2016-09-21 18:56:42 +0200

    ptite modif alakon

--- ft_put_solution.c
+++ ft_put_solution.c
@@ -6,7 +6,7 @@
 /*   By: quroulon <quroulon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/20 14:50:06 by quroulon          #+#    #+#             */
-/*   Updated: 2016/09/21 14:07:00 by quroulon         ###   ########.fr       */
+/*   Updated: 2016/09/21 18:48:23 by quroulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,26 +51,38 @@ void			ft_put_solution(t_lem_in *env, int frm_d, int frm_a, int frm_p)
 	int			len;
 	char		**tab;
 	int			tmp;
+	int			num;
 
 	tab = NULL;
 	len = 0;
 	tmp = 0;
+	num = 0;
 	tab = ft_init_tabpath(env, tab, 0, &len);
 
 	total = env->nb_ant;
 	while (frm_a != env->nb_ant)
 	{
 		i = 0;
+		num = frm_a;
 		if (frm_p < env->nb_ant)
 			frm_p += env->nb_path;
-		while (i < frm_p)
+		if (frm_a == 0)
+			i++;
+		while (i < frm_p && i > 0)
 		{
 			i++;
+			num++;
 			// ft_printf("\n%d, %d\n", frm_a, frm_d);
-			if (tab[frm_p - frm_a - i])
+			if (tab[frm_p - i - 1] && frm_a > 1)
 			{
-				ft_printf("L%d-%s ", i, tab[frm_p - frm_a - i]);
-				ft_printf("%d, %d, %d, %d\n", frm_p, frm_a, i, frm_p - frm_a - i);
+				ft_printf("L%d-%s ", num, tab[frm_p - i - 1]);
+				ft_printf("    p : %d, a : %d, d : %d, m : %d\n", frm_p, frm_a, frm_d, frm_p - frm_a - i);
+
+			}
+			else if (tab[frm_p - frm_a - i])
+			{
+				ft_printf("L%d-%s ", num, tab[frm_p - frm_a - i]);
+				ft_printf("    p : %d, a : %d, d : %d, m : %d\n", frm_p, frm_a, frm_d, frm_p - frm_a - i);
 			}
 			if (frm_p - frm_a - i + 1 == len && frm_a < env->nb_ant)
 				tmp++;
@@ -83,9 +95,12 @@ void			ft_put_solution(t_lem_in *env, int frm_d, int frm_a, int frm_p)
 		// if (frm_p - frm_a - i + 1 == len && frm_a < env->nb_ant)
 		// {
 		// 	frm_p--;
-		// }	
-		frm_d--;
-		ft_printf("\n");
+		// }
+		if (frm_d > 0)
+			frm_d--;
+		ft_printf("    p : %d, a : %d, d : %d, m : %d\n", frm_p, frm_a, frm_d, frm_p - frm_a - i);
+
+		// ft_printf("\n");
 	}
 
 	// while (frm >= 0)
